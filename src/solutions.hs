bc31ca6f9fa05fc631fe8106626f4dcb766bf58e
Anssi Heiska <aheiska@iki.fi>
2016-09-30 15:52:47 +0300

    Slides state: readyish

--- src/solutions.hs
+++ src/solutions.hs
@@ -1,3 +1,5 @@
+-- map
+-------------------------
 
 double :: [Integer] -> [Integer]
 double []     = []
@@ -7,6 +9,11 @@ lens :: [String] -> [Int]
 lens []       = []
 lens (s : ss) = length s : lens ss
 
+map' :: (a -> b) -> [a] -> [b]
+map' _ []       = []
+map' f (x : xs) = f x : map' f xs
+
+-- filter
 ---------------------------
 
 alle5 :: [Int] -> [Int]
@@ -32,5 +39,75 @@ evens (x : xs)
 posFst [] = []
 posFst ((a, b) : xs) = if a > 0 then a : posFst xs else posFst xs
 
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' f (x : xs) = if f x then x : filter f xs else filter f xs
 
 
+-- flatmap
+--------------------------
+
+monista :: [(Int, a)] -> [a]
+monista []                 = []
+monista ((montako, x) : t) = replicate montako x ++ monista t
+
+toista :: [Int] -> [Int]
+toista []      = []
+toista (h : t) = replicate h h ++ toista t
+
+flatmap :: (a -> [b]) -> [a] -> [b]
+flatmap _ []       = []
+flatmap f (a : as) = f a ++ flatmap f as
+
+
+flatten :: [[a]] -> [a]
+flatten []       = []
+flatten (l : ls) = l ++ flatten ls
+
+flatmap' :: (a -> [b]) -> [a] -> [b]
+flatmap' f l = flatten (map' f l)
+-- flatmap' f = flatten.(map' f)
+
+-- saisko |> :n jotenkin kalvoille :)
+-- f#: List.map f |> List.flatten
+
+
+-- fold
+-------------------
+
+summa :: [Integer] -> Integer
+summa []       = 0
+summa (x : xs) = x + summa xs
+
+tulo []       = 1
+tulo (x : xs) = x * tulo xs
+
+cat []       = ""
+cat (s : ss) = s ++ cat ss
+
+
+fold :: (a -> b -> b) -> b -> [a] -> b
+fold f z []       = z
+fold f z (a : as) = f a (fold f z as)
+
+
+-- mapf filterf flatmapf
+--------------------------------
+
+mapf :: (a -> b) -> [a] -> [b]
+mapf f xs = fold (\x acc -> f x : acc) ([] :: [b]) xs
+
+reducef :: (a -> a -> a) -> [a] -> a
+reducef f (x : xs) = fold f x xs
+
+filterf :: (a -> Bool) -> [a] -> [a]
+filterf f xs = fold lf ([] :: [a]) xs where
+  lf x acc
+    | f x       = x : acc
+    | otherwise = acc
+
+filterf' :: (a -> Bool) -> [a] -> [a]
+filterf' f as = fold (\x acc -> (if f x then [x] else []) ++ acc) [] as
+
+flatmapf :: (a -> [b]) -> [a] -> [b]
+flatmapf f l = fold (\a acc -> f a ++ acc) [] l
